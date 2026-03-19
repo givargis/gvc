364dc472a08e4f09f86ded006465853cdf3c64fe
Tony Givargis <tony.givargis@gmail.com>
2026-03-19 15:15:57 -0700

    minor edits

--- src/g_version.h
+++ src/g_version.h
@@ -5,7 +5,7 @@
 
 #define G_VERSION_NAME "gvc"
 
-#define G_VERSION_COPYRIGHT "Copyright (c) Tony Givargis, 2024-202"
+#define G_VERSION_COPYRIGHT "Copyright (c) Tony Givargis, 2024-2026"
 
 #define G_VERSION_MAJOR 1
 #define G_VERSION_MINOR 1
